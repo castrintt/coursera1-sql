591d59f6ea3e3379c61e78c544cc927d4e90aa19
castrintt <igordc38@gmail.com>
2026-04-17 20:18:30 -0300

    refactor: increase bcrypt hash rounds for enhanced password security
    - Updated bcrypt hash rounds from 10 to 12 in the PasswordToHash utility to improve password hashing strength.

--- src/shared/utils/passwordToHash.ts
+++ src/shared/utils/passwordToHash.ts
@@ -2,6 +2,6 @@ import * as bcrypt from 'bcrypt';
 
 export class PasswordToHash {
     public static async hash(password: string): Promise<string> {
-        return await bcrypt.hash(password, 10);
+        return await bcrypt.hash(password, 12);
     }
 }
